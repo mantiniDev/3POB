23cd998782fc55d3d5ecf27012ee96edb1fa1b45
mantiniDev <pmantinidev@gmail.com>
2022-05-11 23:40:16 -0300

    retirei_optionPane

--- Projeto/ProjetoExerc.java
+++ Projeto/ProjetoExerc.java
@@ -1,6 +1,5 @@
 package Projeto;
 
-
 import javax.swing.JOptionPane;
 
 /**
@@ -10,22 +9,17 @@ import javax.swing.JOptionPane;
 public class ProjetoExerc {
 
     public static void main(String[] args) {
-        String nomeUsuario =  JOptionPane.showInputDialog("Digite seu nome");
-        
+        String nomeUsuario = JOptionPane.showInputDialog("Digite seu nome");
+
         String idadeString = JOptionPane.showInputDialog("Qual sua idade?");
         int idade = Integer.parseInt(idadeString);
-        
-        String alturaString = JOptionPane.showInputDialog("Qual sua altura?");
-        double altura = Double.parseDouble(alturaString);
 
         String alturaString = JOptionPane.showInputDialog("Qual sua altura?");
         double altura = Double.parseDouble(alturaString);
-        
-        
-        
-        
-        JOptionPane.showMessageDialog (null,"Meu nome é "+ nomeUsuario + " e tenho " + idade +" anos de idade.\nTenho " + altura + " de altura.","Resposta",2);
-        
-        
+
+        JOptionPane.showMessageDialog(null,
+                "Meu nome é " + nomeUsuario + " e tenho " + idade + " anos de idade.\nTenho " + altura + " de altura.",
+                "Resposta", 2);
+
     }
 }
