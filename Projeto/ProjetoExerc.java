64c37742dcc7cedbdbfc590d198733d33c12f7f8
mantiniDev <pmantinidev@gmail.com>
2022-05-11 22:57:23 -0300

    exercicio3_slideOracle

---
+++ Projeto/ProjetoExerc.java
@@ -0,0 +1,27 @@
+package Projeto;
+
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Paulo
+ */
+public class ProjetoExerc {
+
+    public static void main(String[] args) {
+        String nomeUsuario =  JOptionPane.showInputDialog("Digite seu nome");
+        
+        String idadeString = JOptionPane.showInputDialog("Qual sua idade?");
+        int idade = Integer.parseInt(idadeString);
+        
+        String alturaString = JOptionPane.showInputDialog("Qual sua altura?");
+        double altura = Double.parseDouble(alturaString);
+        
+        
+        
+        JOptionPane.showMessageDialog (null,"Meu nome é "+ nomeUsuario + " e tenho " + idade +" anos de idade.\nTenho " + altura + " de altura.","Resposta",2);
+        
+        
+    }
+}
